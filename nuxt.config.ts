efc263db3b0b52c4d6ca276f1ca9e216a7c6f34a
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-12-30 19:13:36 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    head: {
+      charset: 'utf-16',      
+      viewport: 'width=device-width, initial-scale=1',      
+      title: 'AliExpress clone | Ecommerce w/ Nuxt 3 Vue js Tailwind CSS Supabase Prisma Netlify',  
+      script: [
+        { src: 'https://js.stripe.com/v3/', defer: true }
+      ]    
+    }
+  },
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    'nuxt-icon',
+    'nuxt-lodash',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase'
+  ],
+  runtimeConfig: {
+    public: {
+      stripPk: process.env.STRIPE_PK_KEY || process.env.PK_KEY,
+    }
+  }    
 })
