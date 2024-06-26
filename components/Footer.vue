bbc83189c6ec6a0911d950855bb457f9bd19adff
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-06-26 23:14:58 +0200

    intégration w/ tailwindcss

---
+++ components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+        
+        <div class="md:flex gap-4 justify-between w-full">
+            <div class="md:w-[50%] pt-4">
+                <div class="text-lg font-semibold text-[#282828]">Help</div>
+                <div class="text-[#99999] text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vitae dicta sunt expedita</div>
+            </div>
+            <div class="md:w-[50%] pt-4">
+                <div class="text-lg font-semibold text-[#282828]">AliExpress multi langages</div>
+                <div class="text-[#99999] text-sm">france, espagnol, russie, allemand, danois, anglais</div>
+            </div>
+        </div>
+        <div class="md:flex gap-4 justify-between w-full">
+            <div class="md:w-[50%] pt-4">
+                <div class="text-lg font-semibold text-[#282828]">Help</div>
+                <div class="text-[#99999] text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vitae dicta sunt expedita</div>
+            </div>
+            <div class="md:w-[50%] pt-4">
+                <div class="text-lg font-semibold text-[#282828]">AliExpress multi langages</div>
+                <div class="text-[#99999] text-sm">france, espagnol, russie, allemand, danois, anglais</div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <div id="FooterSub" class="w-full bg-[#33333] text-sm text-sm text-gray-400">
+    <div class="max-w-[1200px] mx-auto p-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, sunt
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
+<script setup>
+    import MainLayout from '~/layouts/MainLayout.vue';
+</script>
 <template>
     <MainLayout>
         <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
             IP
-            </div>
+        </div>
     </MainLayout>
 </template>
-
-<script setup>
-    import MainLayout from '~/layouts/MainLayout.vue';
-</script>
-
 <style>
 
 </style>
